f5f0d3a0726227f5c530f6c541188574f57b9559
Chielvl <chielvanleeuwen@hotmail.com>
2022-01-20 12:13:06 +0100

    eerste commit bericht

---
+++ CSharpBackEnd/Controllers/GerechtController.cs
@@ -0,0 +1,44 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CSharpBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GerechtController : ControllerBase
+    {
+        // GET: api/<GerechtController>
+        [HttpGet]
+        public IEnumerable<string> Get() 
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GerechtController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GerechtController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GerechtController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GerechtController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ClassLibrary1/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) { }
+
+        public DbSet<Gerecht> gerecht { get; set; }
+
+    }
+}
