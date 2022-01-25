72e582a95886a861be7e1eea517c25865736ac67
Chielvl <chielvanleeuwen@hotmail.com>
2022-01-25 18:19:34 +0100

    Controllers geupdate

--- CSharpBackEnd/Controllers/ProductenController.cs
+++ CSharpBackEnd/Controllers/ProductenController.cs
@@ -1,7 +1,10 @@
 ﻿using ClassLibrary1;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpBackEnd.Controllers
 {
@@ -25,11 +28,18 @@ namespace CSharpBackEnd.Controllers
         }
 
         // GET api/<ProductenController>/5
-        [HttpGet("{id}")]
+        [HttpGet("{productId}")]
         public Product GetProduct(int productId)
         {
             var product = databaseContext.Find<Product>(productId);
 
+            return product;
+        }
+
+        [HttpGet("find/{productNaam}")]
+        public Product GetProductByName(string productNaam)
+        {
+            var product = databaseContext.producten.Where(p => p.Naam == productNaam).Single();
             return product;
         }
 
@@ -44,6 +54,23 @@ namespace CSharpBackEnd.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        private bool CompareNames(string name1, string name2)
+        {
+            int missedChars = 0;
+
+            for(int i =0 ; i<name1.Length; i++)
+            {
+                if(name1[i] != name2[i])
+                    missedChars++;
+            }
+
+            if(missedChars <= 2)
+            
+                return true;
+            
+            return false;
         }
     }
 }
